be1a16c43917ca64bc7de328db743ec6d79dd53b
Alvin <alvinz_lai@hotmail.com>
2022-12-14 14:02:21 +1000

    Entries added

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ import express from 'express'
 
 const categories =['Food', 'Coding', 'Work', 'Other']
 
+const entries = [
+    {category: 'Food', content: 'Hello'},
+    {category: 'Coding', content: 'Express is cool'},
+    {category: 'Work', content: 'Another day at the office'}
+]
+
 const app = express()
 const port = 4001
 
+app.use(express.json())
+
 app.get('/', (request, response) => response.send({info: 'Journal API'}))
 
-app.get('/categories', (req, res) => res.status(200).send(categories))
+app.get('/categories', (req, res) => res.send(categories))
+
+app.get('/entries', (req, res) => res.send(entries))
+
+app.get('/entries/:id', (req, res) => {
+    const entry = entries[req.params.id]
+    if (entry) {
+        res.send(entry)
+    } else {
+        res.status(404).send({ error: 'Entry not Found' })
+    }
+    
+})
+
+app.post('/entries', (req, res) => {
+    // 1. Create a new entry objects with values passed in from the request
+    const { category, content } = req.body
+    const newEntry = { category, content }
+    // 2. Create a new entry to the entries array
+    entries.push(newEntry)
+    // 3. Send the new entry with 201 status
+    res.status(201).send(newEntry)
+})
+
+
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`))
